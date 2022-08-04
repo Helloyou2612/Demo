134142ae8e69e6e1ee4d1bf4cbb5aad0a5d8f0c7
helloyou2612 <helloyou2612@gmail.com>
2022-08-04 21:20:58 +0700

    Read json data from file txt and show data in View

---
+++ DemoFilePicker/DemoFilePicker/DataModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DemoFilePicker
+{
+    public class DataModel
+    {
+        public List<User> Users { get; set; }
+    }
+
+    public class User
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string AuthType { get; set; }
+        public string Salt { get; set; }
+        public string PassHash { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public string CreatedBy { get; set; }
+        public object UpdatedDate { get; set; }
+        public object UpdatedBy { get; set; }
+    }
+}
---
+++ DemoFilePicker/DemoFilePicker/MainPage.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+using Newtonsoft.Json;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace DemoFilePicker
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void BtnOpen_OnClick(object sender, RoutedEventArgs e)
+        {
+            var openPicker = new FileOpenPicker()
+            {
+                ViewMode = PickerViewMode.Thumbnail,
+                SuggestedStartLocation = PickerLocationId.ComputerFolder
+            };
+
+            openPicker.FileTypeFilter.Add(".txt");
+
+            //openPicker.FileTypeFilter.Add("*");
+
+            var file = await openPicker.PickSingleFileAsync();
+            if (file != null)
+            {
+                using (var stream = await file.OpenStreamForReadAsync())
+                using (var reader = new StreamReader(stream))
+                {
+                    var fileContent = await reader.ReadToEndAsync();
+                    var data = JsonConvert.DeserializeObject<List<User>>(fileContent);
+                }
+            }
+            else
+            {
+                var dialog = new MessageDialog("Vui lòng chọn 1 file ảnh.");
+                await dialog.ShowAsync();
+            }
+
+        }
+    }
+}
