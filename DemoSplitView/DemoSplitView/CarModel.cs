dde91d5061082cda98d4c9864cfc79b900467a1c
helloyou2612 <helloyou2612@gmail.com>
2022-08-04 20:03:16 +0700

    commit code

---
+++ DemoSplitView/DemoSplitView/CarModel.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DemoSplitView
+{
+    public class CarModel
+    {
+        public string Category { get; set; }
+        public string Type { get; set; }
+        public string Model { get; set; }
+        public string Image { get; set; }
+    }
+
+    public class CarManager
+    {
+        public static List<CarModel> GetData()
+        {
+            var cars = new List<CarModel>
+            {
+                new CarModel
+                {
+                    Category = "BMW",
+                    Type = "MPV",
+                    Model = "2019",
+                    Image = "/Assets/Cars/bmw.jpeg"
+                },
+                new CarModel
+                {
+                    Category = "Honda Civic",
+                    Model = "2019",
+                    Type = "Sedan",
+                    Image = "/Assets/Cars/civic.jpeg"
+                },
+                new CarModel
+                {
+                    Category = "MayBach",
+                    Model = "2019",
+                    Type = "Sedan",
+                    Image = "/Assets/Cars/maybach.jpg"
+                },
+                new CarModel
+                {
+                    Category = "VinFast",
+                    Model = "2018",
+                    Type = "HatchBack",
+                    Image = "/Assets/Cars/Vinfast.jpeg"
+                },
+                new CarModel
+                {
+                    Category = "Volvo-XC40",
+                    Model = "2021",
+                    Type = "MPV",
+                    Image = "/Assets/Cars/Volvo-XC40-white-scaled.jpg"
+                }
+            };
+            return cars;
+        }
+
+        public static List<CarModel> GetCars(string type)
+        {
+            return GetData().Where(m => m.Type == type).ToList();
+        }
+    }
+}
---
+++ DemoSplitView/DemoSplitView/MainPage.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.ObjectModel;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace DemoSplitView
+{
+    /// <summary>
+    ///     An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private readonly ObservableCollection<CarModel> Cars;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            Cars = new ObservableCollection<CarModel>();
+        }
+
+        private void BtnHamburger_OnClick(object sender, RoutedEventArgs e)
+        {
+            MySplitView.IsPaneOpen = !MySplitView.IsPaneOpen;
+        }
+
+        private void BtnMVP_OnClick(object sender, RoutedEventArgs e)
+        {
+            var mpvCars = CarManager.GetCars("MPV");
+            
+            Cars.Clear();
+            foreach (var item in mpvCars)
+            {
+                Cars.Add(item);
+            }
+        }
+
+        private void BtnHatchBack_OnClick(object sender, RoutedEventArgs e)
+        {
+            var hatchBackCars = CarManager.GetCars("HatchBack");
+
+            Cars.Clear();
+            foreach (var item in hatchBackCars)
+            {
+                Cars.Add(item);
+            }
+        }
+
+        private void BtnSedan_OnClick(object sender, RoutedEventArgs e)
+        {
+            var sedanCars = CarManager.GetCars("Sedan");
+
+            Cars.Clear();
+            foreach (var item in sedanCars)
+            {
+                Cars.Add(item);
+            }
+        }
+
+        private void GridView_OnItemClick(object sender, ItemClickEventArgs e)
+        {
+        }
+    }
+}
